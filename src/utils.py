d3c61c9c4626fb289d7a14910f47ac031884bd2a
AriBora <arindom21@iitk.ac.in>
2023-06-12 22:51:44 +0530

    Update

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import torch
-from transformers import pipeline
+# from transformers import pipeline
 from src.logger import logging
 from src.exception import CustomException
 
@@ -19,41 +19,41 @@ class BART:
         self.tokenizer=BartTokenizer.from_pretrained(model_name)
         self.model=BartForConditionalGeneration.from_pretrained(model_name)
     
-    # def summarize(self,text, max_length=500):
-    #     words = text.split(' ')
-    #     words =set(words)
-    #     max_len = min(len(words),1024)
-    #     logging.info('Total words :{}'.format(max_len))
-    #     inputs = self.tokenizer.batch_encode_plus([text], max_length=max_len, return_tensors='pt', truncation=True)
-    #     input_ids = inputs['input_ids']
-    #     attention_mask = inputs['attention_mask']
-    #     logging.info("Words encoded. Now generating summary...")
-    #     summary_ids = self.model.generate(input_ids, attention_mask=attention_mask, min_length=max_length//2, max_length=max_length)
-    #     summary = self.tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+    def summarize(self,text, max_length=500):
+        words = text.split(' ')
+        words =set(words)
+        max_len = min(len(words),1024)
+        logging.info('Total words :{}'.format(max_len))
+        inputs = self.tokenizer.batch_encode_plus([text], max_length=max_len, return_tensors='pt', truncation=True)
+        input_ids = inputs['input_ids']
+        attention_mask = inputs['attention_mask']
+        logging.info("Words encoded. Now generating summary...")
+        summary_ids = self.model.generate(input_ids, attention_mask=attention_mask, min_length=max_length//2, max_length=max_length)
+        summary = self.tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
 
-    #     return summary
+        return summary
 
-    def summarize(self, text, max_length=500):
-        hf_name = 'pszemraj/led-base-book-summary'
-        summarizer = pipeline(
-            "summarization",
-            hf_name,
-            device=0 if torch.cuda.is_available() else -1,
-        )
+    # def summarize(self, text, max_length=500):
+    #     hf_name = 'pszemraj/led-base-book-summary'
+    #     summarizer = pipeline(
+    #         "summarization",
+    #         hf_name,
+    #         device=0 if torch.cuda.is_available() else -1,
+    #     )
 
-        result = summarizer(
-            text,
-            min_length = 8,
-            max_length = max_length,
-            no_repeat_ngram_size=3,
-            encoder_no_repeat_ngram_size=3,
-            repetition_penalty=3.5,
-            num_beams=4,
-            do_sample=False,
-            early_stopping=True,
-        )
+    #     result = summarizer(
+    #         text,
+    #         min_length = 8,
+    #         max_length = max_length,
+    #         no_repeat_ngram_size=3,
+    #         encoder_no_repeat_ngram_size=3,
+    #         repetition_penalty=3.5,
+    #         num_beams=4,
+    #         do_sample=False,
+    #         early_stopping=True,
+    #     )
 
-        return result[0]
+    #     return result[0]
 
 class Embeddings:
     def __init__(self):
